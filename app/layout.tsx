4c940838793a618c8388828e6a3c199ef0aa89c6
GarrettBreard <100252661+GarrettBreard@users.noreply.github.com>
2023-11-06 20:41:29 -0600

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+import Nav from "@/components/layout/nav";
+import Footer from "@/components/layout/footer";
+import { Suspense } from "react";
+
+export const metadata = {
+  title: "AgTrac - CS361 Project to visualize Ag Data",
+  description:
+    "Ag Trac is Garrett's project utiilizing USDA's REST API to visualize agricultural data ",
+  
+  metadataBase: new URL("https://google.com"),
+  themeColor: "#FFF",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+        <Suspense fallback="...">
+          {/* @ts-expect-error Server Component */}
+          <Nav />
+        </Suspense>
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+          {children}
+        </main>
+        <Footer />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
---
+++ components/layout/footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
+      <p className="text-gray-500">
+        Using Firebase, React, Vercel and the Precedent Vercel Template by:
+        <a
+          className="font-medium text-gray-800 underline transition-colors"
+          href="https://twitter.com/steventey"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Garrett Breard
+        </a>
+      </p>
+    </div>
+  );
+}
